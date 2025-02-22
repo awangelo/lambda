fc200975f6ae1f3cf6a8833aec99bda065ce898d
awangelo <anggg.gaio@gmail.com>
2025-02-22 10:27:14 -0300

    formatacao com 2 spaces

--- DefinicaoECondicionais.hs
+++ DefinicaoECondicionais.hs
@@ -3,32 +3,35 @@ module DefinicaoECondicionais where
 -- `let` define variaveis locais, pode ser usado dentro expressoes
 -- Tem escopo na expressao que foi criada
 foo :: (Ord a, Num a) => a -> a
-foo x = if x > 10
-        then let min = 2
-             in x * min
-        else let max = 5
-             in x * max
-
+foo x =
+  if x > 10
+    then
+      let min = 2
+       in x * min
+    else
+      let max = 5
+       in x * max
 
 -- `where` define variaveis locais, deve final de funcoes
 -- Tem escopo de funcao
 bar :: (Ord a, Num a) => a -> a
-bar x = if x > 10
-        then x * min
-        else x * max
-    where min = 2
-          max = 5
-
+bar x =
+  if x > 10
+    then x * min
+    else x * max
+  where
+    min = 2
+    max = 5
 
 -- exemplo de guards, a funcao pode ter multiplas condicoes
 -- `otherwise` eh sempre o ultimo caso, ele eh apenas um `True`
 baz :: (Ord a, Num a) => a -> a
-baz x 
-    | x > 10 = x * min
-    | otherwise = x * max
-    where min = 2
-          max = 5
-
+baz x
+  | x > 10 = x * min
+  | otherwise = x * max
+  where
+    min = 2
+    max = 5
 
 -- A primeira condicao que for verdadeira vai ser executada
 odeioZeros :: (Eq p, Num p) => p -> p -> p
